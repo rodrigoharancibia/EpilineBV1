abd41fd358ce17bdb6c3cd765055911614d0dfea
Seu Nome <rodrigo.h.arancibia@gmail.com>
2024-11-07 20:36:02 -0300

    Activating  The Ncbi download

--- setting.sh
+++ setting.sh
@@ -1,6 +1,6 @@
 
 
-#wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.16.0+-x64-linux.tar.gz
+wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.16.0+-x64-linux.tar.gz
 tar -xvf ncbi-blast-2.16.0+-x64-linux.tar.gz
 
 
